c56c4d9256d39abf905280faa600b70ca99cc155
Michael Song <logydoghan@hotmail.com>
2017-02-15 16:58:14 -0800

    Add files via upload

---
+++ app/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var restRouter = require('./routes/rest');
+var redirectRouter = require('./routes/redirect');
+var indexRouter = require('./routes/index');
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://logydoghan:33068doghan@ds011278.mlab.com:11278/tinyurl");
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.use("/api/v1", restRouter);
+
+app.use("/", indexRouter);
+
+app.use("/:shortUrl", redirectRouter);
+
+app.listen(3000);
